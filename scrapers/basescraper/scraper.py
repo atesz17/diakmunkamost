9fe94f849dbb3dda4fc531471bb036641dc04d72
atesz17-laptop <atesz17@laptop.hu>
2016-02-23 12:30:15 +0100

    some initial modification in basecraper

--- scrapers/basescraper/scraper.py
+++ scrapers/basescraper/scraper.py
@@ -1,11 +1,29 @@
+import configparser
+
 import requests
 
 
 class BaseScraper:
+    '''
+    Minden scrapernek ez lesz ay ososztalya
+    Lehetosing szerint az automatizalhato dolgokat mint pl.: url-ek scrapelese,
+    json kiirasa, cache osszehasonlitasa, ezeket itt megvlaositom, a
+    specifikusabb reszeket pedig majd az alosztalyban. Lenyeges dolgokat
+    egy config fajlbol fogja kiolvasni
+    '''
 
-    def __init__(self, name, all_job_url):
-        self.name = name
-        self.all_job_url = all_job_url
+    def __init__(self, config_file="scraper.ini"):
+        self.__read_configuration(configparser.ConfigParser(config_file))
+
+    def __read_configuration(self, config):
+        '''
+        Azoknal az ertekeknel, ami mindenkeppen specifikus, ott a [] operatort
+        kell hasznalni, ahol elkepzelheto deafult value, ott a get()-et
+        '''
+        self.name = self.config['Name']
+        self.all_job_url = self.config['AllJobUrl']
+        self.cache = self.config.get('Cache', 'cache.html')
+        self.jobs_json = self.config.get('JSON', 'scraped_jobs.json')
 
     def scrape(self):
         pass
