066f8a984ac5af19880744219034660390860751
Attila <atesz@ubun.tu>
2016-02-24 15:30:49 -0800

    start to implement scrape command

--- scrapers/management/commands/scrape.py
+++ scrapers/management/commands/scrape.py
@@ -1,11 +1,33 @@
 from django.core.management.base import BaseCommand, CommandError
 
+from scrapers.apps import ScrapersConfig
+
+import importlib
 
 class Command(BaseCommand):
     help = 'Starts scraping the specified job page'
 
     def handle(self, *args, **options):
-        scrapers = self.get_scrapers()
+        """
+        A szep megoldas az lenne, ahogy django source-ban is van ld:
+        execute_from_command_line hogy mukodik
+        ugy lenne szep, hogy mindegyik scraper package-tol megkoveteljuk
+        hogy a scraper neveben szerepeljen a 'scraper' szo (mint a testnel)
+        es automatikusan megtalalja azokat a packageket/modulokat,
+        beimportalja es vegrehajtja oket. Most egyelore az apps.py-ban
+        meg kell adni a scraper package-k nevet
+        """
+        for package_name in ScrapersConfig.scrapers:
+            scraper_module = ''
+            try:
+                scraper_module = importlib.import_module(
+                    "scrapers.{0}.scraper".format(package_name)
+                )
+            except ImportError:
+                print('No package name was found by the name: {0}, '
+                      'check scrapers/apps.py'.format(package_name))
+                continue
 
-    def get_scrapers(self):
-        pass
+
+
+
