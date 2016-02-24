33a70aa1b3418f2352b34064396c8ba21c3343e6
Attila <atesz@ubun.tu>
2016-02-24 14:51:50 -0800

    remove unnecessary code

--- scrapers/management/commands/scrape.py
+++ scrapers/management/commands/scrape.py
@@ -6,13 +6,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         scrapers = self.get_scrapers()
-        '''
-        for scraper in options['scrape']:
-            if scraper == "ydiak":
-                YDiakScraper().scrape
-            else:
-                raise CommandError("{0} named scraper doesnt exist")
-        '''
 
     def get_scrapers(self):
         pass
