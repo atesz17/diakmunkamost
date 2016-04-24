be3fdee7162f1598deada369a4f85de086fd9cbf
Attila <me@attila.hi>
2016-04-24 20:52:24 +0200

    Customized Scrapers admin page

--- scrapers/admin.py
+++ scrapers/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import URL, State, Provider
 
-admin.site.register(URL)
+
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'state', 'provider')
+    list_filter = ('state', 'provider')
+    ordering = ('url', 'state', 'provider')
+    search_fields = ('url',)
+
+admin.site.register(URL, UrlAdmin)
 admin.site.register(State)
 admin.site.register(Provider)
