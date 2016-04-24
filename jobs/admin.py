45abd77b93b088a5123a842313e6d12199e60051
Attila <me@attila.hi>
2016-04-24 20:44:13 +0200

    Customized Jobs admin page

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import Job, JobType, JobProvider
 
-admin.site.register(Job)
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'job_type', 'min_salary')
+    list_filter = ('job_type',)
+    ordering = ('title',)
+    search_fields = ('title',)
+
+admin.site.register(Job, JobAdmin)
 admin.site.register(JobType)
 admin.site.register(JobProvider)
